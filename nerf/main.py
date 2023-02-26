2c84cd5cdcacf5adfecbfc0ef46560b1a1634527
AlBenetti <benettialessandro96@gmail.com>
2023-02-26 22:19:41 +0100

    Added test files for pre commit hooks

--- nerf/main.py
+++ nerf/main.py
@@ -1,2 +1,15 @@
+"""Main module for nerf."""
 import numpy as np
 import pandas as pd
+
+
+def main():
+    """Main function for nerf."""
+    random_array = np.random.rand(10, 10)
+    random_df = pd.DataFrame(random_array)
+
+    print(random_df.mean())
+
+
+if __name__ == '__main__':
+    main()
---
+++ test/tests.py
@@ -0,0 +1,4 @@
+"""Run tests for the project."""
+def run_tests():
+    """Run tests for the project."""
+    return 1
