4c428791f4c2b3992e6c5586ec8af7fb3c24f112
AlBenetti <benettialessandro96@gmail.com>
2023-02-26 23:08:34 +0100

    Removed pandas dependency

--- nerf/main.py
+++ nerf/main.py
@@ -1,14 +1,12 @@
 """Main module for nerf."""
 import numpy as np
-import pandas as pd
 
 
 def main():
     """Main function for nerf."""
     random_array = np.random.rand(10, 10)
-    random_df = pd.DataFrame(random_array)
 
-    print(random_df.mean())
+    print(random_array.mean())
 
 
 if __name__ == '__main__':
