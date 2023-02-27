5b442a6727b7941d063abc76aae4126dd9b9148e
benettia <alessandro.benetti@prometeia.com>
2023-02-27 22:06:27 +0100

    added mypy for testing

--- test/tests.py
+++ test/tests.py
@@ -1,12 +1,11 @@
 """Run tests for the project."""
 import numpy as np
-import pandas as pd
 
 def run_tests():
     """Run tests for the project."""
     return 1
 
-def check_array_equal(a, b):
+def check_array_equal(a: np.ndarray, b: np.ndarray) -> bool:
     """Check if two arrays are equal."""
     return np.array_equal(a, b)
 
